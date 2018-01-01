c54847b6214fec508c17e6262718c5a6438b376f
JensenHome\Nathan <nathanjensen.music@gmail.com>
2018-01-01 14:26:49 -0700

    Arrow Sticks into Material

---
+++ Assets/Arrows/ArrowBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehavior : MonoBehaviour {
+
+	private bool inFlight = true;
+	private Rigidbody rb;
+
+	public PhysicMaterial canStickTo;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(rb.velocity.magnitude > 5 && inFlight) {
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rb.velocity), 0.05f);
+		}
+	}
+
+	void StickTo (GameObject parent) {
+		inFlight = false;
+		print (parent.GetComponent<Collider>().material);
+		if(parent.GetComponent<Collider>().material.name == "canStickTo (Instance)") {
+			rb.isKinematic = true;
+			transform.parent = parent.transform;
+			transform.localPosition += transform.forward*0.2f;
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if(inFlight) {
+			StickTo(collider.gameObject);
+		}
+	}
+}
